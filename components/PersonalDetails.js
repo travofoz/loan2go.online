7bcabae2ed2e667de1047efa7606779c704b7a05
travofoz <travis@lotek.org>
2024-10-14 04:14:44 -0400

    valid?

--- components/PersonalDetails.js
+++ components/PersonalDetails.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 const PersonalDetails = () => {
+    /* old way. we dont do this now. 
+    
     const [formData, setFormData] = useState({
         first_name: '',
         last_name: '',
@@ -9,7 +11,7 @@ const PersonalDetails = () => {
         phone: '',
         active_military: 'NO',
         dob: ''
-    });
+    }); */
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -34,6 +36,7 @@ const PersonalDetails = () => {
         if (storedActiveMilitary) setFormData(storedActiveMilitary);
     }, []);
 
+
     const validate = (name, value) => {
         let error = '';
         switch (name) {
@@ -48,7 +51,7 @@ const PersonalDetails = () => {
                 break;
             case 'phone':
                 if (!value) error = 'This field is required';
-                else if (!/^\d{10}$/.test(value)) error = 'Invalid phone number';
+                else if (!/^\d{10}$/.test(value) || !/^\d{3}-\d{3}-\d{4}$/.test(value)) error = 'Invalid phone number';
                 break;
             default:
                 break;
@@ -80,9 +83,16 @@ const PersonalDetails = () => {
     };
 
     const handleBlur = (e) => {
-        const { name, value, type, checked } = e.target;
-        localStorage.setItem(name,value);
+        const { name, value } = e.target;
+        const error = validate(name, value);
+        setErrors({ ...errors, [name]: error });
+        setTouched({ ...touched, [name]: true });
+        localStorage.setItem(name, value);
+    };
+
     
+    const isFormValid = () => {
+        return Object.keys(errors).every(key => !errors[key]);
     };
 
     return (
@@ -90,40 +100,40 @@ const PersonalDetails = () => {
             <h2 className="text-lg font-semibold text-zinc-900 mb-4 capitalize">Step 2/6: Personal Details</h2>
             <div>
                 <label htmlFor="first_name" className="block text-sm font-black text-zinc-900">First Name</label>
-                <input name="first_name" value={firstName} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.first_name && errors.first_name ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
+                <input name="first_name" value={formData.first_name} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.first_name && errors.first_name ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
                 {touched.first_name && errors.first_name && <p className="text-red-500 text-xs italic">{errors.first_name}</p>}
             </div>
             <div>
-            <label htmlFor="last_name" className="block text-sm font-black text-zinc-900">Last Name</label>
-                <input name="last_name" value={lastName} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.last_name && errors.last_name ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
+                <label htmlFor="last_name" className="block text-sm font-black text-zinc-900">Last Name</label>
+                <input name="last_name" value={formData.last_name} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.last_name && errors.last_name ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
                 {touched.last_name && errors.last_name && <p className="text-red-500 text-xs italic">{errors.last_name}</p>}
             </div>
             <div>
                 <label htmlFor="email" className="block text-sm font-black text-zinc-900">Email</label>
-                <input type="email" name="email" value={email} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.email && errors.email ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
+                <input type="email" name="email" value={formData.email} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.email && errors.email ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
                 {touched.email && errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
             </div>
             <div>
                 <label htmlFor="phone" className="block text-sm font-black text-zinc-900">Phone</label>
-                <input type="tel" name="phone" value={phone} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.phone && errors.phone ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
+                <input type="tel" name="phone" value={formData.phone} onChange={handleChange} onBlur={handleBlur} required className={`mt-1 p-2 block w-full rounded-md border-2 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white ${touched.phone && errors.phone ? 'border-red-500' : 'border-zinc-700 border-opacity-20'}`} />
                 {touched.phone && errors.phone && <p className="text-red-500 text-xs italic">{errors.phone}</p>}
             </div>
             <div>
                 <label htmlFor="dob" className="block text-sm font-black text-zinc-900">Date of Birth</label>
-                <input type="date" name="dob" value={dob} onChange={handleChange} required className="mt-1 p-2 block w-full rounded-md border-zinc-700 border-2 border-opacity-20  shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white"/>
+                <input type="date" name="dob" value={formData.dob} onChange={handleChange} required className="mt-1 p-2 block w-full rounded-md border-zinc-700 border-2 border-opacity-20  shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white"/>
+                {touched.dob && errors.dob && <p className="text-red-500 text-xs italic">{errors.dob}</p>}
             </div>
             <div>
                 <label htmlFor="active_military" className="block text-sm font-black text-zinc-900">Active Military</label>
-                <select name="active_military" value={activeMilitary} onChange={handleChange} required className="mt-1 p-2 block w-full rounded-md border-zinc-700 border-2 border-opacity-20  shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white">
+                <select name="active_military" value={formData.active_military} onChange={handleChange} required className="mt-1 p-2 block w-full rounded-md border-zinc-700 border-2 border-opacity-20  shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 bg-white">
                     <option value="NO">No</option>
                     <option value="YES">Yes</option>
                 </select>
             </div>
-            <Link href="/address-details" className={`w-auto flex justify-center py-2 px-4 border-2 rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 ${Object.keys(errors).some(key => errors[key]) ? 'pointer-events-none opacity-50' : 'border-green-300'}`}>
+            <Link href="/address-details" className={`w-auto flex justify-center py-2 px-4 border-2 rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 ${!isFormValid() ? 'pointer-events-none opacity-50' : 'border-green-300'}`}>
                 Next
             </Link>
         </form>
     );
 };
-
 export default PersonalDetails;
